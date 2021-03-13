45837d4257498177adb268ad03f8513a0134ee31
anthony <ac.nicholls@gmail.com>
2021-03-12 23:05:59 -0500

    logic fixes, add nlog

--- pdf-combine/Func/Split.cs
+++ pdf-combine/Func/Split.cs
@@ -20,15 +20,20 @@ namespace pdf_combine.Func
             
             try
             {
-                string basePath = Path.GetFullPath(inputFilename);
+                string basePath = Path.GetDirectoryName(inputFilename);
                 string filePath = Path.GetFileName(inputFilename);
-                var outputFolderName = Path.Combine(basePath, filePath);
-                var oldFile = PdfReader.Open(Path.Combine(outputFolderName, inputFilename), PdfDocumentOpenMode.ReadOnly);
+                DirectoryInfo outputDir = new DirectoryInfo(Path.Combine(basePath, filePath));
+                if(!outputDir.Exists)
+                {
+                    outputDir.Create();
+                }
+                var oldFile = PdfReader.Open(inputFilename, PdfDocumentOpenMode.ReadOnly);
+
                 foreach (var page in pageListRows)
                 {
                     PdfDocument newPage = new PdfDocument();
                     newPage.AddPage(oldFile.Pages[page]);
-                    newPage.Save(Path.Combine(outputFolderName, "Page" + (page + 1)));
+                    newPage.Save(Path.Combine(outputDir.FullName, "Page" + (page + 1)));
                 }
             }
             catch (Exception x)
