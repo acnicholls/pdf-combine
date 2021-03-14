dbd4751145f30741e660b8d836b62b56c0483fc3
anthony <ac.nicholls@gmail.com>
2021-03-13 23:43:32 -0400

    adds about form

--- pdf-combine/Forms/frmAbout.Designer.cs
+++ pdf-combine/Forms/frmAbout.Designer.cs
@@ -29,12 +29,94 @@ namespace pdf_combine.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.lblCopyright = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.Location = new System.Drawing.Point(12, 20);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(487, 51);
+            this.lblDescription.TabIndex = 0;
+            this.lblDescription.Text = "DESCRIPTION: This program combines and splits pdf files to a user\'s specification" +
+    ".";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(12, 56);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(87, 15);
+            this.lblVersion.TabIndex = 1;
+            this.lblVersion.Text = "Version : 2.0.0.0";
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Location = new System.Drawing.Point(12, 85);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(106, 15);
+            this.lblAuthor.TabIndex = 2;
+            this.lblAuthor.Text = "Author : acnicholls";
+            // 
+            // lblCopyright
+            // 
+            this.lblCopyright.AutoSize = true;
+            this.lblCopyright.Location = new System.Drawing.Point(12, 117);
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(93, 15);
+            this.lblCopyright.TabIndex = 3;
+            this.lblCopyright.Text = "Copyright : 2021";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(403, 160);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "OK";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Please use this software responsibly.";
+            // 
+            // frmAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAbout";
+            this.ClientSize = new System.Drawing.Size(511, 222);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblCopyright);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblDescription);
+            this.MaximizeBox = false;
+            this.Name = "frmAbout";
+            this.Text = "About pdf-combine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label lblCopyright;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
     }
 }
